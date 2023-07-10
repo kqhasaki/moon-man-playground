7357f9547c3f212bb252140708523d6276ff58bd
kqhasaki <k1664032884@gmail.com>
2023-07-10 19:53:55 +0800

    feat: improve WeaponCard style

--- src/components/elden-ring/WeaponCard/index.tsx
+++ src/components/elden-ring/WeaponCard/index.tsx
@@ -1,5 +1,5 @@
 import { Image } from "@kqhasaki/birdperson";
-import { ReactElement } from "react";
+import { ReactElement, useCallback, useRef } from "react";
 
 import { Weapon } from "../../../api/elden-ring/weapon";
 import { makeStyles } from "../theme";
@@ -7,13 +7,13 @@ import { makeStyles } from "../theme";
 const useStyles = makeStyles()((theme) => ({
   cardWrapper: {
     boxSizing: "border-box",
-    height: 640,
+    height: 400,
     width: 360,
-    padding: 20,
     color: theme.palette.text.secondary,
-    border: `1px solid ${theme.palette.divider}`,
-    background: theme.palette.background.paper,
     fontFamily: "serif",
+    perspective: 1000,
+  },
+  inner: {
     "& h3": {
       textAlign: "center",
       marginTop: 24,
@@ -21,15 +21,46 @@ const useStyles = makeStyles()((theme) => ({
       color: theme.palette.text.gold,
       fontSize: 25,
     },
+    border: `1px solid ${theme.palette.divider}`,
+    background: theme.palette.background.paper,
+    position: "relative",
+    width: "100%",
+    height: "100%",
+    textAlign: "center",
+    transition: "transform 0.5s",
+    transformStyle: "preserve-3d",
   },
   description: {
-    margin: 0,
-    height: "200px",
+    height: "100%",
     width: "100%",
     overflowY: "auto",
     overflowX: "hidden",
-    fontSize: 18,
+    fontSize: 20,
     lineHeight: 1.3,
+    display: "flex",
+  },
+  front: {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+    "-webkit-backface-visibility": "hidden",
+    backfaceVisibility: "hidden",
+    padding: 20,
+  },
+  back: {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+    "-webkit-backface-visibility": "hidden",
+    backfaceVisibility: "hidden",
+    transform: "rotateY(180deg)",
+    padding: 20,
+  },
+  imageWrapper: {
+    height: "unset",
+    width: "90%",
+    aspectRatio: "1 / 1",
+    margin: "auto",
   },
 }));
 
@@ -39,20 +70,44 @@ type WeaponCardPropsType = {
 
 export function WeaponCard({ data }: WeaponCardPropsType): ReactElement {
   const { classes } = useStyles();
+  const inner = useRef<HTMLDivElement>(null);
+
+  const turnBack = useCallback(() => {
+    if (!inner.current) {
+      return;
+    }
+    inner.current.style.transform = "rotateY(180deg)";
+  }, []);
+
+  const turnFront = useCallback(() => {
+    if (!inner.current) {
+      return;
+    }
+    inner.current.style.transform = "";
+  }, []);
 
   return (
-    <article className={classes.cardWrapper}>
-      <Image
-        src={data.image}
-        alt={data.image}
-        style={{
-          width: "100%",
-          height: "unset",
-          aspectRatio: "1 / 1",
-        }}
-      />
-      <h3>{data.name}</h3>
-      <p className={classes.description}>{data.description}</p>
+    <article className={classes.cardWrapper} onMouseLeave={turnFront}>
+      <section className={classes.inner} ref={inner}>
+        <div className={classes.front}>
+          <div className={classes.imageWrapper} onMouseEnter={turnBack}>
+            <Image
+              src={data.image}
+              alt={data.image}
+              style={{
+                height: "100%",
+                width: "100%",
+              }}
+            />
+          </div>
+          <h3>{data.name}</h3>
+        </div>
+
+        <div className={classes.back}>
+          <h3>{data.name}</h3>
+          <p className={classes.description}>{data.description}</p>
+        </div>
+      </section>
     </article>
   );
 }
